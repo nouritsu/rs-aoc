733811a001635fff14517ea5b489537106955155
Aneesh <aneesh1701@gmail.com>
2023-07-20 23:48:51 +0530

    feature(day-2): read and print input file

--- 2022/day-2/src/main.rs
+++ 2022/day-2/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use common::functions::{get_input_file_arg, read_input};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let f = get_input_file_arg();
+
+    println!("{:?}", read_input(&f)?);
+    Ok(())
 }
