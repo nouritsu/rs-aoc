3c4c7b0696fabee1ec8e915b55b32ae342f1adcf
Aneesh <aneesh1701@gmail.com>
2023-07-28 13:40:51 +0530

    feat(day-5): implement solution to part 1

--- 2022/day-5/src/main.rs
+++ 2022/day-5/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod r#move;
+
+use common::functions::{get_input_file_arg, read_input};
+use r#move::MoveCrate;
+use std::{io, str::FromStr};
+
+fn main() -> io::Result<()> {
+    let f = get_input_file_arg();
+    let input = read_input(&f)?;
+
+    let crates = vec![
+        vec!['S', 'Z', 'P', 'D', 'L', 'B', 'F', 'C'],
+        vec!['N', 'V', 'G', 'P', 'H', 'W', 'B'],
+        vec!['F', 'W', 'B', 'J', 'G'],
+        vec!['G', 'J', 'N', 'F', 'L', 'W', 'C', 'S'],
+        vec!['W', 'J', 'L', 'T', 'P', 'M', 'S', 'H'],
+        vec!['B', 'C', 'W', 'G', 'F', 'S'],
+        vec!['H', 'T', 'P', 'M', 'Q', 'B', 'W'],
+        vec!['F', 'S', 'W', 'T'],
+        vec!['N', 'C', 'R'],
+    ];
+
+    let mut crates_mut = crates.clone();
+    input
+        .iter()
+        .map(|s| MoveCrate::from_str(s).expect("invalid input"))
+        .for_each(|m| m.apply(&mut crates_mut));
+    let output1 = crates_mut
+        .iter()
+        .map(|v| v.last().expect("invalid input").to_owned())
+        .collect::<String>();
+
+    println!("Tops of crates after moves: {}", output1);
+
+    Ok(())
 }
---
+++ 2022/day-5/src/move.rs
@@ -0,0 +1,53 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct MoveCrate {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+
+#[derive(Debug)]
+pub struct ParseMoveError;
+
+impl FromStr for MoveCrate {
+    type Err = ParseMoveError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let numbers = s
+            .split(' ')
+            .filter(|s| s.chars().all(|c| c.is_numeric()))
+            .map(|s| s.parse::<usize>().map_err(|_| ParseMoveError))
+            .collect::<Result<Vec<usize>, ParseMoveError>>()?;
+
+        if numbers.len() != 3 {
+            return Err(ParseMoveError);
+        }
+
+        Ok(MoveCrate::new(numbers[0], numbers[1] - 1, numbers[2] - 1))
+    }
+}
+
+impl MoveCrate {
+    pub fn new(count: usize, from: usize, to: usize) -> Self {
+        MoveCrate { count, from, to }
+    }
+
+    pub fn apply(&self, v: &mut Vec<Vec<char>>) {
+        for _ in 0..self.count {
+            let e = v[self.from].pop().expect("invalid input");
+            v[self.to].push(e);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_str() {
+        let m = MoveCrate::from_str("move 2 from 5 to 9").unwrap();
+        assert_eq!(MoveCrate::new(2, 4, 8), m);
+    }
+}
