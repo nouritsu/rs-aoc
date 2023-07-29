6c8dc266ecc48fff4d89104b6cc36543ebbb1c6d
Aneesh <aneesh1701@gmail.com>
2023-07-29 23:26:20 +0530

    feat(functions): add read_input_str function

--- common/src/functions.rs
+++ common/src/functions.rs
@@ -15,3 +15,7 @@ pub fn get_input_file_arg() -> String {
 pub fn read_input(p: &impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(p)?).lines().collect()
 }
+
+pub fn read_input_str(p: &impl AsRef<Path>) -> io::Result<String> {
+    io::read_to_string(File::open(p)?)
+}
